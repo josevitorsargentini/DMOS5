7280f17c3e357d0857e897289db9d7e59b11f527
José Vitor Sargentini <dudd.vitor@gmail.com>
2024-03-13 11:26:05 -0300

    Aula 13/03/2024 "Localizando recursos"

--- Projeto1/app/src/main/java/br/edu/ifsp/arq/projeto1/MainActivity.kt
+++ Projeto1/app/src/main/java/br/edu/ifsp/arq/projeto1/MainActivity.kt
@@ -1,20 +1,28 @@
 package br.edu.ifsp.arq.projeto1
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var edtName: TextView
+    lateinit var btnSend: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+
+
+        edtName = findViewById(R.id.edt_name)
+        btnSend = findViewById(R.id.btn_send)
+
+        btnSend.setOnClickListener(View.OnClickListener {
+            val nome: String = edtName.text.toString()
+            android.widget.Toast.makeText(this, getString(R.string.message) + " " + nome,
+                android.widget.Toast.LENGTH_SHORT).show()
+        })
     }
 }
